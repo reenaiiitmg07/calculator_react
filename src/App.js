2a330eed71a3510313de8719ac520a03a58102a1
yadavsunil00001 <yadavsunil00001@gmail.com>
2018-09-14 20:05:05 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import Appcss from './App.css';
 class App extends Component {
   constructor(props) {
 	      super(props);
-	      this.state =null;
-
-	       this.handleClick = this.handleClick.bind(this);
+	      this.state ={
+          val1:0,
+          val2:0,
+          opp:null,
+          res:0
+        }
+      	       this.handleClick = this.handleClick.bind(this);
 	   }
 
- handleClick() {
-	 console.log("hi");
+ handleClick(e) {
+   let val2=this.state.val2;
+   let val1=this.state.val1;
+   let opp=this.state.opp;
+   let result=this.state.res;
+   if(opp==null&&e.target.value)
+   if()
  }
 
   render() {
@@ -26,10 +35,10 @@ class App extends Component {
 
 	    </div>
             <div className="row">
-	      <div className="col-sm-3 cal-col" onClick={this.handleClick}>7</div>
-	      <div className="col-sm-3 cal-col" onClick={this.handleClick}>8</div>
+	      <div className="col-sm-3 cal-col"><button className="btn" value={7} onClick={this.handleClick}>7</button></div>
+	      <div className="col-sm-3 cal-col"><button className="btn" value={8} onClick={this.handleClick}>8</button></div>
               <div className="col-sm-3 cal-col" onClick={this.handleClick}>9</div>
-              <div className="col-sm-3 cal-col">*</div>
+              <div className="col-sm-3 cal-col"><button className="btn" value={'*'} onClick={this.handleClick}>*</button></div>
 
 	    </div>
             <div className="row">
