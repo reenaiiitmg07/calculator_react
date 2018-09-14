f05c6ebf82b023b783bc5e51ee837b115e5b39ae
reenavermaiiitm <reenavermaiiitm@gmail.com>
2018-09-15 02:13:28 +0530

    updaate

--- src/App.js
+++ src/App.js
@@ -7,27 +7,68 @@ class App extends Component {
           val1:0,
           val2:0,
           opp:null,
-          res:0
+          res:null
         }
       	       this.handleClick = this.handleClick.bind(this);
+               this.handleResult=this.handleResult.bind(this);
+               this.allClear=this.allClear.bind(this);
 	   }
 
+allClear(e){
+  this.setState({
+    val1:0,
+    val2:0,
+    opp:null,
+    res:null
+  });
+}
+handleResult(e){
+ let val1=this.state.val1;
+ let val2=this.state.val2;
+ let opp=this.state.opp;
+ let res;
+ console.log(val1);
+ console.log(val2);
+ console.log(opp);
+
+ if(opp==='*')
+ {
+   res=val1*val2;
+   console.log(res);
+ }
+ else if(opp==='+'){
+   res=val1+val1;
+ }
+ else if(opp=='-'){
+   res=val1-val2;
+ }
+ else {
+   res=val1/val2;
+ }
+ this.state.res=res;
+ //console.log(res);
+}
+
  handleClick(e) {
-   let val2=this.state.val2;
    let val1=this.state.val1;
+   let val2=this.state.val2;
    let opp=this.state.opp;
    let result=this.state.res;
-   if(opp===null &&(e.target.value!='+'||e.target.value!='-'||e.target.value!='*'||e.target.value!='/')){
+   if(opp===null &&(e.target.value!='+' && e.target.value!='-'&&e.target.value!='*'&&e.target.value!='/')){
      val1=val1*10+parseInt(e.target.value);
      this.setState({val1:val1});
-     console.log(val1);
+     //console.log(val1);
    }
    else {
-     console.log(e.target.value);
     if(opp==null && (e.target.value=='+'||e.target.value=='-'||e.target.value=='*'||e.target.value=='/') ){
       opp=e.target.value;
-      this.setState({val1:val1});
-      console.log(opp);
+      this.setState({opp:opp});
+      //console.log(opp);
+    }
+    else{
+      val2=val2*10+parseInt(e.target.value);
+      this.setState({val2:val2});
+      //console.log(val2);
     }
    }
    //console.log(val1);
@@ -37,38 +78,42 @@ class App extends Component {
     return (
       <div className="App">
 	    <div className="result">
-              result
+
+          {this.state.opp?this.state.val2:this.state.val1}
+
+
+
 	    </div>
 	   <div className="row">
-	      <div className="col-sm-3 cal-col">AC</div>
+	      <div className="col-sm-3 cal-col"><button className="btn" onClick={this.allClear}>AC</button></div>
 	      <div className="col-sm-3 cal-col">+/-</div>
               <div className="col-sm-3 cal-col">%</div>
-              <div className="col-sm-3 cal-col">/</div>
+              <div className="col-sm-3 cal-col"><button className="btn"value='/' onClick={this.handleClick}>/</button></div>
 
 	    </div>
             <div className="row">
 	      <div className="col-sm-3 cal-col"><button className="btn" value={7} onClick={this.handleClick}>7</button></div>
 	      <div className="col-sm-3 cal-col"><button className="btn" value={8} onClick={this.handleClick}>8</button></div>
-              <div className="col-sm-3 cal-col" onClick={this.handleClick}>9</div>
-              <div className="col-sm-3 cal-col"><button className="btn" value='*' onClick={this.handleClick}>*</button></div>
+        <div className="col-sm-3 cal-col"><button className="btn"value={9} onClick={this.handleClick}>9</button></div>
+        <div className="col-sm-3 cal-col"><button className="btn" value='*' onClick={this.handleClick}>*</button></div>
 
 	    </div>
             <div className="row">
-	      <div className="col-sm-3 cal-col"onClick={this.handleClick}>4</div>
-	      <div className="col-sm-3 cal-col"onClick={this.handleClick}>5</div>
-              <div className="col-sm-3 cal-col"onClick={this.handleClick}>6</div>
-              <div className="col-sm-3 cal-col">-</div>
+	      <div className="col-sm-3 cal-col"><button className="btn"value={4} onClick={this.handleClick}>4</button></div>
+	      <div className="col-sm-3 cal-col"><button className="btn"value={5} onClick={this.handleClick}>5</button></div>
+        <div className="col-sm-3 cal-col"><button className="btn"value={6} onClick={this.handleClick}>6</button></div>
+        <div className="col-sm-3 cal-col"><button className="btn"value='-' onClick={this.handleClick}>-</button></div>
 
 	    </div>
-            <div className="row">
-	      <div className="col-sm-3 cal-col"onClick={this.handleClick}>1</div>
-	      <div className="col-sm-3 cal-col"onClick={this.handleClick}>2</div>
-              <div className="col-sm-3 cal-col"onClick={this.handleClick}>3</div>
-              <div className="col-sm-3 cal-col">+</div>
+      <div className="row">
+	      <div className="col-sm-3 cal-col"><button className="btn"value={1} onClick={this.handleClick}>1</button></div>
+	      <div className="col-sm-3 cal-col"><button className="btn"value={2} onClick={this.handleClick}>2</button></div>
+        <div className="col-sm-3 cal-col"><button className="btn"value={3} onClick={this.handleClick}>3</button></div>
+        <div className="col-sm-3 cal-col"><button className="btn"value='+' onClick={this.handleClick}>+</button></div>
 	       <div className="row">
-	      <div className="col-sm-6 cal-col"onClick={this.handleClick}>0</div>
+	      <div className="col-sm-6 cal-col"><button className="btn"value={0} onClick={this.handleClick}>0</button></div>
               <div className="col-sm-3 cal-col">.</div>
-              <div className="col-sm-3 cal-col">=</div>
+              <div className="col-sm-3 cal-col"><button className="btn"onClick={this.handleResult}>=</button></div>
 
 	    </div>
 
